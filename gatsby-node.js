902ca97caea2fc7cb9a839b43b86747e2874db3d
juanjo <jorbegob@emeal.nttdata.com>
2024-06-20 00:31:19 -0500

    style: change redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,8 @@
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage, createRedirect } = actions
-
-  createRedirect({ fromPath: '/about', toPath: '/about2' })
-
-
-  /* const result = await graphql(`
-    query {
-      allContentfulBlogPost {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-    }
-    `)
-    result.data.allContentfulBlogPost.edges.forEach(({ node }) => {
-      createPage({
-        path: `/blog/${node.slug}`,
-        component: path.resolve(`./src/templates/blog-post.js`),
-        context: {
-          slug: node.slug,
-        },
-      })
-    }
-    ) */
+exports.createPages = async ({ actions }) => {
+  const { createRedirect } = actions
+  createRedirect({
+    fromPath: '/about', toPath: '/about2',
+    redirectInBrowser: true,
+    isPermanent: true,
+  })
 }
